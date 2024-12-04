cfd8c1cdfda856e961f39a58ec3db141c5ef880f
MinhHien1234 <165647011+MinhHien1234@users.noreply.github.com>
2024-12-04 11:25:03 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Components/Redux/store";
+import "./App.css"; 
+import ProductsPage from "./Components/ProductsPage";
+import AddProductPage from "./Components/AddProductPage";
+import EditProductPage from "./Components/EditProductPage";
+import HuongDan from "./Components/HuongDan";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="app-container">
+         
+          <div className="sidebar">
+            <div>
+            <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active-link" : "")}
+              >
+                Hướng dẫn
+              </NavLink>
+              
+              <NavLink
+                to="/products"
+                className={({ isActive }) => (isActive ? "active-link" : "")}
+              >
+              Quản lý hàng hóa
+              </NavLink>
+             
+            </div>
+          </div>
+      
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<HuongDan />} />
+              <Route path="/huong-dan" element={<HuongDan />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/add-product" element={<AddProductPage />} />
+              <Route path="/edit-product/:id" element={<EditProductPage />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
